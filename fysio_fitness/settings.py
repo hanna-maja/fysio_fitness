65117d0754bfe8748b962dbd1ccff820b9ef0e9e
hanna-maja <hanna-maja@sewen.se>
2021-01-02 21:50:54 +0000

    FIX: static files served in heroku

--- fysio_fitness/settings.py
+++ fysio_fitness/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'rl79(n)t*wzi2r=yo!osr6b31ou)1r^jo6ub58gh8gx%h_%&9!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fysio-fitness.herokuapp.com', 'fysiofitness.nu']
+ALLOWED_HOSTS = ['fysio-fitness.herokuapp.com', 'fysiofitness.nu', 'localhost']
 
 
 # Application definition
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fysio_fitness.urls'
@@ -152,6 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
