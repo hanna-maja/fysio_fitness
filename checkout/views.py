f48d177e05f6b8ba53910bcbafa5f605d5a04cd1
hanna-maja <hanna-maja@sewen.se>
2021-01-04 19:46:13 +0000

    FIX: made checkout-redirect to videos

--- checkout/views.py
+++ checkout/views.py
@@ -40,8 +40,7 @@ def checkout(request):
             
             # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success',
-                                    args=[order.order_number]))
+            return redirect(reverse('videos'))
         else:
             messages.error(request, ('Fel i formuläret'
                                      'Vänligen dubbelkolla informationen du angivit.'))
@@ -93,46 +92,6 @@ def checkout(request):
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
         'plan_id': plan_id,
-    }
-
-    return render(request, template, context)
-
-
-def checkout_success(request, order_number):
-    """
-    Handle successful checkouts
-    """
-    save_info = request.session.get('save_info')
-    order = get_object_or_404(Order, order_number=order_number)
-
-    # if request.user.is_authenticated:
-    #     profile = UserProfile.objects.get(user=request.user)
-    #     # Attach the user's profile to the order
-    #     order.user_profile = profile
-    #     order.save()
-
-    #     # Save the user's info
-    #     if save_info:
-    #         profile_data = {
-    #             'default_phone_number': order.phone_number,
-    #             'default_country': order.country,
-    #             'default_postcode': order.postcode,
-    #             'default_town_or_city': order.town_or_city,
-    #             'default_street_address1': order.street_address1,
-    #             'default_street_address2': order.street_address2,
-    #             'default_county': order.county,
-    #         }
-    #         user_profile_form = UserProfileForm(profile_data, instance=profile)
-    #         if user_profile_form.is_valid():
-    #             user_profile_form.save()
-
-    messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
-
-    template = 'checkout/checkout_success.html'
-    context = {
-        'order': order,
     }
 
     return render(request, template, context)
