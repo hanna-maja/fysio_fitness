b088faa77c6aed8ff3cabdcee87fc3d3d4bc8865
hanna-maja <hanna-maja@sewen.se>
2021-01-05 22:25:07 +0000

    FIX: renamed userprofile to profile

--- users/models.py
+++ users/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     valid_until = models.DateField(null=True, blank=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.user_profile.save()
+    instance.profile.save()
