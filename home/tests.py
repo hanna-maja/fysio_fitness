d6dba308c02a6eb4a5ee4a88b6176e93bd4584b3
hanna-maja <hanna-maja@sewen.se>
2021-01-05 11:27:33 +0000

    TEST: testing cta button in home

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeIndexViewTests(TestCase):
+    def test_slider_ctabutton(self):
+        """
+        CTA-button should link to subscription
+        """
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            '<a class="waves-effect waves-light btn-large" href="/traning/">',
+        )
