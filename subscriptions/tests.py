73e621233993825d7c9b4df5e7c5e9b20da2cbaa
hanna-maja <hanna-maja@sewen.se>
2021-01-05 15:33:24 +0000

    TESTS: added tests for subscriptions

--- subscriptions/tests.py
+++ subscriptions/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import SubscriptionPlan
+from django.core.files import File
 
-# Create your tests here.
+
+class SubscriptionsIndexViewTests(TestCase):
+    def create_plan(self, plan_name, price, days):
+        test_image = File(open("static/images/mela1.jpg", 'rb'))
+        return SubscriptionPlan.objects.create(name=plan_name, 
+            price=100,
+            image=test_image)
+
+    def test_buy_button(self):
+        """
+        monthly-button should link to checkout
+        """
+        self.create_plan("test", 100, 30)
+        response = self.client.get(reverse('subscriptions'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            '/checkout?plan_id=1',
+            1,
+        )
+    
+    def test_title(self):
+        """
+        title should be name of plan
+        """
+        title = "test"
+        self.create_plan(title, 100, 30)
+        response = self.client.get(reverse('subscriptions'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            '<span class="card-title">{}</span>'.format(title),
+            1,
+        )
+
+    def test_button_price(self):
+        """
+        price should be part of buttontext
+        """
+        price = 100.00
+        self.create_plan("test", price, 30)
+        response = self.client.get(reverse('subscriptions'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            '<a class="btn" href="/checkout?plan_id=1">Köp {:.2f} kr</a>'.format(price),
+            1,
+        )
