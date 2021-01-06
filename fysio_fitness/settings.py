f5704fbd6e0e294a356566f7070e1db4eeda6c64
hanna-maja <hanna-maja@sewen.se>
2021-01-06 08:35:12 +0000

    CHORE: Trying to fix postqresql as default database

--- fysio_fitness/settings.py
+++ fysio_fitness/settings.py
@@ -110,18 +110,14 @@ WSGI_APPLICATION = 'fysio_fitness.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+DATABASES = {}
 if development:
-    DATABASES = {
-        'default': {
+    DATABASES['default'] =  {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
 else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+    DATABASES['default'] =  dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 
 # Password validation
